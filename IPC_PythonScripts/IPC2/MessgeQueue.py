3064599e7df690d26e0d8e96c6099e58bd45c02c
pooja-n1424 <poojanallabolu@gmail.com>
2021-07-22 12:25:38 +0530

    Modified Output location for each IPC Method implemented.
    Signed-off-by: pooja-n1424 <poojanallabolu@gmail.com>

--- IPC_PythonScripts/IPC2/MessgeQueue.py
+++ IPC_PythonScripts/IPC2/MessgeQueue.py
@@ -1,40 +1,40 @@
 import logging
-import os
-import pathlib
-import random
-import threading
-import time
+from os import getcwd
+from pathlib import Path
+from random import randint
+from threading import Thread
+from time import sleep
 
 # Importing Queue libraries
 try:
     import queue
 except ImportError:
-    import Queue as queue
+    import Queue as q
 
 logging.basicConfig(level=logging.DEBUG,
                     format='(%(threadName)-9s) %(message)s', )
 
-BUF_SIZE = 20
-q = queue.Queue(BUF_SIZE)
+buffer_size = 20
+q = queue.Queue(buffer_size)
 
 class Utils():
     def create_producer_data_file(self, name):
         no_of_integers_in_a_file = 100;
-        f = open(os.getcwd() + "/Output/Producer/" + name + ".txt", "w")
+        f = open(getcwd() + "/Output/Producer/" + name + ".txt", "w")
         for no_of_lines in range(no_of_integers_in_a_file):
-            f.write(str(random.randint(1, no_of_integers_in_a_file)))
+            f.write(str(randint(1, no_of_integers_in_a_file)))
         f.close()
 
     def create_consumer_data_file(self, name, data):
-        f = open(os.getcwd() + "/Output/Consumer/" + name + ".txt", "w")
+        f = open(getcwd() + "/Output/Consumer/" + name + ".txt", "w")
         f.write(data)
         f.close()
 
-class ProducerThread(threading.Thread):
+class producer_thread(Thread):
     def __init__(self, group=None, target=None, name=None,
                  args=(), kwargs=None, verbose=None):
-        pathlib.Path(os.getcwd() + "/Output/Producer").mkdir(parents=True, exist_ok=True)
-        super(ProducerThread, self).__init__()
+        Path(getcwd() + "/Output/Producer").mkdir(parents=True, exist_ok=True)
+        super(producer_thread, self).__init__()
         self.target = target
         self.name = name
 
@@ -45,19 +45,19 @@ class ProducerThread(threading.Thread):
             if not q.full():
                 utils.create_producer_data_file("producer_data_file_" + str(produced_file_num))
                 item = open(
-                    os.getcwd() + "/Output/Producer/producer_data_file_" + str(produced_file_num) + ".txt", "r")
+                    getcwd() + "/Output/Producer/producer_data_file_" + str(produced_file_num) + ".txt", "r")
                 q.put(item.read())
                 logging.debug('Putting ' + str("producer_data_file_" + str(produced_file_num))
                               + ' : ' + str(q.qsize()) + ' items in queue')
-                time.sleep(1)
+                sleep(1)
                 produced_file_num += 1;
         return
 
-class ConsumerThread(threading.Thread):
+class consumer_thread(Thread):
     def __init__(self, group=None, target=None, name=None,
                  args=(), kwargs=None, verbose=None):
-        pathlib.Path(os.getcwd() + "/Output/Consumer").mkdir(parents=True, exist_ok=True)
-        super(ConsumerThread, self).__init__()
+        Path(getcwd() + "/Output/Consumer").mkdir(parents=True, exist_ok=True)
+        super(consumer_thread, self).__init__()
         self.target = target
         self.name = name
         return
@@ -71,15 +71,15 @@ class ConsumerThread(threading.Thread):
                 utils.create_consumer_data_file("consumer_data_file_" + str(consumed_files), data)
                 logging.debug('Getting ' + str("producer_data_file_" + str(consumed_files))
                               + ' : ' + str(q.qsize()) + ' items in queue')
-                time.sleep(1)
+                sleep(1)
                 consumed_files += 1;
         return
 
 if __name__ == '__main__':
-    p = ProducerThread(name='producer')
-    c = ConsumerThread(name='consumer')
+    p = producer_thread(name='producer')
+    c = consumer_thread(name='consumer')
 
     p.start()
-    time.sleep(2)
+    sleep(2)
     c.start()
-    time.sleep(2)
+    sleep(2)
